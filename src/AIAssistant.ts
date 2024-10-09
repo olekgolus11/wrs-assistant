b547ac6e4ef028c51716632b50b396db80f0bcb7
Olek Golus <olekgolus02@gmail.com>
2024-10-09 22:30:06 +0200

    Add qdrant connection

--- src/AIAssistant.ts
+++ src/AIAssistant.ts
@@ -1,17 +1,31 @@
-import { OpenAI } from "https://esm.sh/@langchain/openai@0.3.5";
+import { ChatOpenAI } from "https://esm.sh/@langchain/openai@0.3.5";
+import QDrantVectorDB from "./QdrantVectorDB.ts";
+import { HumanMessage, SystemMessage } from "https://esm.sh/v135/@langchain/core@0.3.6/messages.js";
 
 class AIAssistant {
-    private openai: OpenAI;
+    private openai: ChatOpenAI;
+    private qdrantVectorDB: QDrantVectorDB;
 
     constructor() {
-        this.openai = new OpenAI({
+        this.openai = new ChatOpenAI({
             model: "gpt-4o-mini",
         });
+        this.qdrantVectorDB = new QDrantVectorDB();
     }
 
-    async getTestResponse(prompt: string): Promise<string> {
-        const completion = await this.openai.invoke(prompt);
-        return completion;
+    async getTestResponse(prompt: string) {
+        const vectorInformation = await this.qdrantVectorDB.searchTestVectors(prompt);
+        const completion = await this.openai.invoke([
+            new SystemMessage(
+                `I will answer the user questions. I have found ${
+                    vectorInformation.length
+                } relevant documents. Their content is: ${vectorInformation.map((doc) => doc.pageContent).join(", ")}`
+            ),
+            new HumanMessage(prompt),
+        ]);
+        const text = completion.content;
+        console.log(text);
+        return text;
     }
 }
 
---
+++ src/QdrantVectorDB.ts
@@ -0,0 +1,47 @@
+import { QdrantVectorStore } from "https://esm.sh/@langchain/qdrant@0.1.0";
+import { OpenAIEmbeddings } from "https://esm.sh/@langchain/openai@0.3.5";
+import { Document } from "https://esm.sh/@langchain/core@0.3.8/documents";
+import process from "node:process";
+
+class QDrantVectorDB {
+    private store: QdrantVectorStore;
+    private embeddings: OpenAIEmbeddings;
+
+    constructor() {
+        this.embeddings = new OpenAIEmbeddings();
+        this.store = new QdrantVectorStore(this.embeddings, {
+            url: process.env.QDRANT_URL,
+            collectionName: process.env.QDRANT_COLLECTION_NAME,
+            apiKey: process.env.QDRANT_VECTOR_DB_API_KEY,
+        });
+    }
+
+    async createTestVectors() {
+        const documents = [
+            new Document({
+                pageContent:
+                    "Uczelnia, na której pytania będę odpowiadał, to Politechnika Łódzka. Użytkownicy mogą mi zadawać pytania jedynie na temat tej uczelni.",
+                metadata: {
+                    title: "Politechnika Łódzka",
+                    description: "Uczelnia techniczna w Łodzi",
+                },
+            }),
+            new Document({
+                pageContent:
+                    "Wydział Politechniki Łódzkiej, na którego pytania będę odpowiadał, to Wydział Elektrotechniki, Elektroniki, Informatyki i Automatyki, w skrócie WEEIA. Użytkownicy mogą mi zadawać pytania jedynie na temat tego wydziału.",
+                metadata: {
+                    title: "Wydział Elektrotechniki, Elektroniki, Informatyki i Automatyki",
+                    description: "Wydział Politechniki Łódzkiej, w skrócie WEEIA",
+                },
+            }),
+        ];
+        await this.store.addDocuments(documents);
+    }
+
+    async searchTestVectors(query: string) {
+        const results = await this.store.similaritySearch(query, 2);
+        return results;
+    }
+}
+
+export default QDrantVectorDB;
