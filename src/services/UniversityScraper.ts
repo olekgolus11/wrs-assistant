0d173c00892e8b524585729d2abd754e8c1f728d
olekgolus11 <olekgolus02@gmail.com>
2024-11-04 23:37:54 +0100

    Update scraper

--- src/services/UniversityScraper.ts
+++ src/services/UniversityScraper.ts
@@ -2,19 +2,28 @@ import { Readability } from "jsr:@paoramen/cheer-reader";
 import * as cheerio from "https://esm.sh/cheerio@1.0.0";
 
 class UniversityScraper {
-    public async getUrlsFromSitemap(sitemapUrl: string): Promise<string[]> {
+    public async getUrlsFromSitemap(sitemapUrl: string) {
         const response = await fetch(sitemapUrl);
         const text = await response.text();
         const $ = cheerio.load(text, {
             xmlMode: true,
         });
 
-        const urls: string[] = [];
+        const urls: {
+            url: string;
+            date: string;
+        }[] = [];
 
-        $("url loc").each((_, element) => {
-            const url = $(element).text();
+        $("url").each((_, element) => {
+            const loc = $(element).find("loc");
+            const lastmod = $(element).find("lastmod");
+            const url = loc.text();
+            const date = lastmod.text();
             if (url) {
-                urls.push(url);
+                urls.push({
+                    url,
+                    date,
+                });
             }
         });
 
