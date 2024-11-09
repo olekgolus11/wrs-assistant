b54a4e7d9d61be806dd9f4f552cdcde97476e567
olekgolus11 <olekgolus02@gmail.com>
2024-11-09 17:20:47 +0100

    Include cors

--- app.ts
+++ app.ts
@@ -2,9 +2,11 @@ import { Application } from "jsr:@oak/oak";
 import { executionLogger } from "./src/middleware/executionLogger.ts";
 import { v1 } from "./src/routes/v1/v1.ts";
 import { healthcheckRouter } from "./src/routes/healthcheck/healthcheck.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(executionLogger);
 
 app.use(v1.routes(), v1.allowedMethods());
